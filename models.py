3f6ebdb7bb0690b58521b5a2166b77244b5ef4d8
iremtektas <tektasirem13@gmail.com>
2025-04-07 16:45:23 +0300

    modelde deadline için feature eklendi

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import date
 
 class Task(BaseModel):
     id: int
     title: str
     completed: bool = False
+    deadline: Optional[date] = None
